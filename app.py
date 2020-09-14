1076061e646874aaf11467755534304a242a9682
Jamila Sisco <jsisco@email.neit.edu>
2020-09-14 10:34:58 -0400

    Editing html and python files

--- app.py
+++ app.py
@@ -1,20 +1,47 @@
 
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow_sqlalchemy import ModelSchema
 from marshmallow import fields
+import sys
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:pmUQjdnk3sQbMsmosJE9@naybrr.ctwclmh06vdt.us-east-2.rds.amazonaws.com:3306/Naybrr'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 
-# A welcome message to test our server
-@app.route('/')
+class DataTest(db.Model):
+    __tablename__ = "test"
+    id = db.Column(db.Integer, primary_key=True)
+    mydata = db.Column(db.Text())
+
+    def __init__ (self, mydata):
+        self.mydata = mydata
+
+
+@app.route("/submit", methods=["POST"])
+def post_to_db():
+    indata = DataTest(request.form['mydata'])
+    data = copy(indata. __dict__ )
+    del data["_sa_instance_state"]
+    try:
+        db.session.add(indata)
+        db.session.commit()
+    except Exception as e:
+        print("\n FAILED entry: {}\n".format(json.dumps(data)))
+        print(e)
+        sys.stdout.flush()
+    return 'Success! To enter more data, <a href="{}">click here!</a>'.format(url_for("enter_data"))
+
+@app.route("/")
+def enter_data(): 
+    return render_template("data.html")
+
+@app.route('/test')
 def index():
     return "<h1>Testing Naybrr Server</h1>"
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
