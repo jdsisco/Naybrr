6a0dcbecfea22703eadc811226db29a42cac8684
Jamila Sisco <jsisco@email.neit.edu>
2020-09-13 22:51:04 -0400

    Adding html and python files

--- app.py
+++ app.py
@@ -1,43 +1,49 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response, jsonify, render_template
+import io
+import pymysql
+from app import app
+from db_config import mysql
+from tables import Results
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+    Username = request.args.get("Username", None)
 
     # For debugging
-    print(f"got name {name}")
+    print(f"got Username {Username}")
 
     response = {}
 
     # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
+    if not Username:
+        response["ERROR"] = "no Username found, please send a Username."
     # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
+    elif str(Username).isdigit():
+        response["ERROR"] = "Username can't be numeric."
     # Now the user entered a valid name
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+        response["MESSAGE"] = f"Welcome {Username} to our awesome platform!!"
 
     # Return the response in json format
     return jsonify(response)
 
 @app.route('/post/', methods=['POST'])
 def post_something():
-    param = request.form.get('name')
+    param = request.form.get('Username')
     print(param)
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
     if param:
         return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
+            "Message": f"Welcome {Username} to our awesome platform!!",
             # Add this option to distinct the POST request
             "METHOD" : "POST"
         })
     else:
         return jsonify({
-            "ERROR": "no name found, please send a name."
+            "ERROR": "no Username found, please send a Username."
         })
 
 # A welcome message to test our server
