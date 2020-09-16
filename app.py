0cd64efe43323859805ee8bfaf5fc263b7ac82ff
Jamila Sisco <jsisco@email.neit.edu>
2020-09-16 15:46:26 -0400

    Adding @app.route calls

--- app.py
+++ app.py
@@ -97,75 +97,67 @@ def login():
 
    
 
-@app.route("/update", methods=["GET","POST"])
-def update_user():
+                
+
+@app.route("/find", methods=["GET","POST"])
+def find():
     try:
-        try:
-            connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-            cursor = connection.cursor()
-            postgres_get_query = """ SELECT account.accountid, username, email, hashpass, 
-            line1, line2, city, state, zip FROM account 
-            INNER JOIN customeraddress on customeraddress.accountid = account.accountid 
-            WHERE account.accountid = %s; """
-            current_account = ('6')
-            cursor.execute(postgres_get_query, current_account)
-            connection.commit()
-            count = cursor.rowcount
-            credentials = json.dumps(cursor.fetchall())
-            resp = jsonify(success=True)
-            print (credentials)
-            return resp
-                
-        except (Exception, psycopg2.Error) as error :
-            if(connection):
-                print("Failed to retrieve record", error)
-                resp = jsonify(success=False)
-                return resp
-
-        finally:
-            #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
-    except:
-        print("Failed to connect")
-        xconn = jsonify(success=False)
-        return xconn
-
-    try:        #Need to connect update status with pulled account information.
-        try:
-            connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-            cursor = connection.cursor()
-            postgres_update_query =  """WITH updateneighbor AS (
-                UPDATE account SET username = %s, email = %s, hashpass = %s WHERE Username = %s
-                RETURNING accountid)
-                UPDATE customeraddress SET line1 = %s, line2 = %s, city = %s, state = %s, zip = %s 
-                WHERE accountid FROM updateneighbor = accountid;"""
+        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cursor = connection.cursor()
+        postgres_get_query = """ SELECT account.accountid, username FROM account 
+        INNER JOIN customeraddress on customeraddress.accountid = account.accountid 
+        WHERE zip = %s; """
+        search_zip = ('02201')
+        cursor.execute(postgres_get_query, search_zip)
+        connection.commit()
+        count = cursor.rowcount
+        credentials = json.dumps(cursor.fetchall())
+        resp = jsonify(success=True)
+        print (credentials)
+        return resp
             
-            record_to_insert = ('test4', '6th@testemail.com', 'asdff', 'test4', '48 Lois Lane', empty, 'Warwick','RI','02499')
-            cursor.execute(postgres_update_query, record_to_insert)
-            connection.commit()
-            count = cursor.rowcount
-            credentials = json.dumps(cursor.fetchall())
-            resp = jsonify(success=True)
-            print (credentials)
+    except (Exception, psycopg2.Error) as error :
+        if(connection):
+            print("Failed to find zip code", error)
+            resp = jsonify(success=False)
             return resp
-                
-        except (Exception, psycopg2.Error) as error :
-            if(connection):
-                print("Failed to update record", error)
-                resp = jsonify(success=False)
-                return resp
 
-        finally:
-            #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
 
+@app.route("/nearby",methods=["POST"])
+def user_inventory():
+    resp = jsonify(success=True) #Return itemID, name, description, price, quantity, imagePath
+    return resp
+
+@app.route("/insert",methods=["GET"])
+def add_item():
+    resp = jsonify(success=True)
+    return resp
+
+@app.route("/UpdateItem",methods=["POST"])
+def update_item():
+    resp = jsonify(success=True)
+    return resp
+
+@app.route("/delete",methods=["POST"])
+def delete_item():
+    resp = jsonify(success=True)
+    return resp
+
+@app.route("/item",methods=["GET"])
+def find_item():
+    resp = jsonify(success=True) #Return itemID, name, description, price, quantity, imagePath (single?)
+    return resp
+
+@app.route('/test')
+def index():
+    return "<h1>Testing Naybrr Server</h1>"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
