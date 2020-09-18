9f387ed2dab2a4c163744bde2340a5137743bb5f
Jamila Sisco <jsisco@email.neit.edu>
2020-09-18 16:06:06 -0400

    Adding json calls

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 import os
 import psycopg2
 from psycopg2 import Error
- from psycopg2.extras import RealDictCursor
+from psycopg2.extras import RealDictCursor
 
 DATABASE_URL = os.environ['DATABASE_URL']
 #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
